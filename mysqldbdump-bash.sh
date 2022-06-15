9a88805ce05f4c5c56af72e89afe7bb5534599f3
Mate Gogiberidze <mgogiberidze@europebet.com>
2022-06-15 18:02:00 +0400

    update mysqldbdump

--- mysqldbdump-bash.sh
+++ mysqldbdump-bash.sh
@@ -5,6 +5,7 @@ umask 0077
 DATE=$(date +%F-%H-%M-%S)
 OUTPUT_DIR=/home/deploy
 OUTPUT_FILE=${OUTPUT_DIR}/DBBACKUPS/proddb_dump_${DATE}.sql
+OUTPUT_FILE_DIR=${OUTPUT_DIR}/DBBACKUPS/proddb_dump_${DATE}.sql.gz
 OUTPUT_NAME_GZ=proddb_dump_${DATE}.sql.gz
 ACCESS_TOKEN=`cat /tmp/token.txt`
 
@@ -17,5 +18,5 @@ else
 	gzip -f ${OUTPUT_FILE}
 	scp ${OUTPUT_FILE}".gz" [username]@[ServerIP]:/mnt/volume_sfo3_01/dbbackups/.
 	### Upload file to google drive
-	curl -X POST -L -H "Authorization: Bearer "$ACCESS_TOKEN""  -F 'metadata={name : "'"$OUTPUT_NAME_GZ"'"};type=application/json;charset=UTF-8' -F "file=@$OUTPUT_FILE;type=application/zip" "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart"
+	curl -X POST -L -H "Authorization: Bearer "$ACCESS_TOKEN""  -F 'metadata={name : "'"$OUTPUT_NAME_GZ"'"};type=application/json;charset=UTF-8' -F "file=@$OUTPUT_FILE_DIR;type=application/zip" "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart"
 fi
